876e6ffb5da3f409ed94346566b6e62a0f23e304
yi-yang <yi-yang@users.noreply.github.com>
2026-01-01 16:03:02 +0000

    deploy: c6d18e74d3e1d6cce4a2cc7f5c573ca8d73a933e

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -6,14 +6,26 @@ var store = [{
         "teaser": ""
       },{
         "title": "所有文章",
-        "excerpt":"                              欢迎访问我的新网站                                                                                           less than 1 minute read                                欢迎来到我的新个人网站，采用 Jekyll 和 Minimal Mistakes 主题构建！         ","categories": [],
+        "excerpt":"Jekyll文章格式示例 less than 1 minute read 这是一篇使用 Jekyll 创建的文章 欢迎访问我的新网站 less than 1 minute read 欢迎来到我的新个人网站，采用 Jekyll 和 Minimal Mistakes 主题构建！ Hello World less than 1 minute read You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You...","categories": [],
         "tags": [],
         "url": "/posts/",
+        "teaser": ""
+      },{
+        "title": "Hello World",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2023/07/17/hello-world.html",
         "teaser": ""
       },{
         "title": "欢迎访问我的新网站",
         "excerpt":"欢迎来到我的新个人网站，采用 Jekyll 和 Minimal Mistakes 主题构建！   很高兴与你分享这个全新设计。网站已经完全重新设计，拥有更专业的外观和更好的用户体验。   网站特色   新网站包含以下功能：      响应式设计：在桌面、平板和移动设备上都有出色的体验   简洁布局：易于阅读和导航   博客板块：我会在这里分享想法和更新   关于页面：了解更多关于我的信息   接下来的计划   我很快会添加更多内容，包括：      关于我的项目和兴趣的博客文章   作品集展示   更多资源和链接   敬请期待！     由 Jekyll 和 Minimal Mistakes 主题构建  ","categories": ["更新"],
         "tags": ["jekyll","minimal mistakes","网站更新"],
         "url": "/%E6%9B%B4%E6%96%B0/2026/01/01/welcome-to-my-site.html",
+        "teaser": ""
+      },{
+        "title": "Jekyll文章格式示例",
+        "excerpt":"这是一篇使用 Jekyll 创建的文章   这是正文内容。我们使用 Markdown 语法来排版，非常方便易读。      列表项 1   列表项 2   更多内容   欢迎访问 Jekyll 官网 了解更多信息。   ","categories": ["技术"],
+        "tags": ["jekyll","markdown"],
+        "url": "/%E6%8A%80%E6%9C%AF/2026/01/01/test.html",
         "teaser": ""
       }]
