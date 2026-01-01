7d77a00e4df8c99b626de9f086148a0656a2ea3a
yi-yang <yi-yang@users.noreply.github.com>
2026-01-01 14:39:29 +0000

    deploy: d8d831a91464aed37c0d4c7f1bda72d21326a4f5

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Welcome to My New Website",
+        "excerpt":"Welcome to my new personal website powered by Jekyll and the Minimal Mistakes theme! I’m excited to share this new design with you. The website has been completely redesigned with a professional look and better user experience. Features This new website includes: Responsive Design: Works great on desktop, tablet, and...","categories": ["update"],
+        "tags": ["jekyll","minimal mistakes","update"],
+        "url": "/update/2026/01/01/welcome-to-my-site.html",
+        "teaser": ""
+      }]
